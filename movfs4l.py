e8dcf6d26faf0903e840e2860677c82759075b1c
A.J. Venter <ajventer@gmail.com>
2018-05-23 01:46:48 +0200

    New version

--- movfs4l.py
+++ movfs4l.py
@@ -7,7 +7,8 @@ import json
 
 
 PATHS={
-    "mo": '{PREFIX}/drive_c/MO',
+    "mods": '{PREFIX}/drive_c/MO/mods',
+    "profiles": '{PREFIX}/drive_c/MO/profiles',
     "skyrim": '{PREFIX}/drive_c/Steam/steamapps/common/Skyrim Special Edition',
     "plugins.txt": '{PREFIX}/drive_c/users/metalpoet/Local Settings/Application Data/Skyrim Special Edition/Plugins.txt'
 }
@@ -27,10 +28,26 @@ def traverse(rootDir, dirsonly=False):
                 yield os.path.join(dirName.replace(rootDir,''),fname) 
                 
 
-def updatelink(src, dest):
+def updatelink(src, dest, log):
     if os.path.islink(dest):
         os.unlink(dest)
+    elif os.path.exists(dest):
+        shutil.move(dest,'%s.UNVFS' %dest)
+        print ('Backing up ',dest)
+        log['backups'].append(dest)
+    log['links'].insert(0, dest)
+    print ('Linking "%s" to "%s"' %(src,dest))
     os.symlink(src, dest)
+
+def mktree(root, path, log):
+    tree = root
+    for p in path.split('/'):
+        tree = os.path.join(tree,p)
+        if not os.path.isdir(tree):
+            log['dirs'].insert(0,tree)
+            print ('Creating directory ',tree)
+            os.mkdir(tree)
+
 
 def pathHdlr (p):
     return p.replace('{PREFIX}', PREFIX)
@@ -47,49 +64,42 @@ def unvfs(p):
         if os.path.isdir(d):
             print ('Removing directory ', d)
             shutil.rmtree(d)
-    log = {'dirs': [], 'links': []}
+    for b in log.get('backups', []):
+        shutil.move(b,b.replace('.UNVFS',''))
+    log = {'dirs': [], 'links': [], 'backups': []}
     open('movfs4l_log.json','w').write(json.dumps(log, indent=4))
 
 
-def dirsthatexist(p):
-    updirs = {}
-    upfiles = {}
-    for item in traverse(p):
-        if os.path.isdir(item):
-            updirs[item.upper()] = item
-        else:
-            upfiles[item.upper()] = item
-    return updirs, upfiles
 
 def addvfslayer(p,l, log):
-    updirs, upfiles = dirsthatexist(p)
     for item in traverse(l, True):
-        src = os.path.join(l, item)
-        dest = os.path.join(p, item)
-        if not os.path.exists(dest):
-            if not item.upper() in list(updirs.keys()):
-                print ('Creating directory: ',dest)
-                os.makedirs(dest)
-                log['dirs'].append(dest)
-            else:
-                src = os.path.join(p,updirs[item.upper()])
-                print ('Linking directory "%s" to "%s"' %(src, dest))
-                os.symlink(src , dest)
-                log['links'].append(dest)
+        mktree(p, item.lower(), log)  
     for item in traverse(l):
         src = os.path.join(l, item)
-        dest = os.path.join(p, item)
+        dest = os.path.join(p, item.lower())
         if not os.path.isdir(src):
-            if item.upper() in list(upfiles.keys()):
-                dest = os.path.join(p,upfiles[item.upper()])
-            print ('Updating link from "%s" to "%s"' %(src,dest))
-            updatelink(src, dest)
-            log['links'].append(dest)
+            updatelink(src, dest, log)
+
+def lowertree(dir):
+    # renames all subforders of dir, not including dir itself
+    def rename_all( root, items):
+        for name in items:
+            try:
+                os.rename( os.path.join(root, name), 
+                                    os.path.join(root, name.lower()))
+            except OSError:
+                pass # can't rename it, so what
+
+    # starts from the bottom so paths further up remain valid after renaming
+    for root, dirs, files in os.walk( dir, topdown=False ):
+        rename_all( root, dirs )
+        rename_all( root, files)
 
 
 if __name__ == '__main__':
-    log = {'dirs': [], 'links': []}
+    log = {'dirs': [], 'links': [], 'backups': []}
     DATADIR=os.path.join(pathHdlr(PATHS['skyrim']),'Data')
+    lowertree(DATADIR)
     MO_PROFILE='Default'
     if len(sys.argv) > 1:
         MO_PROFILE=sys.argv[1]
@@ -99,14 +109,13 @@ if __name__ == '__main__':
     if MO_PROFILE == 'UNVFS':
         sys.exit()
 
-    PDIR=os.path.join(pathHdlr(PATHS['mo']),'profiles')
-    PDIR=os.path.join(PDIR,MO_PROFILE)
+    PDIR=os.path.join(pathHdlr(PATHS['profiles']),MO_PROFILE)
     PLUGINS=os.path.join(PDIR,'plugins.txt')
     print ('Setting symlink from "%s" to "%s" for loadorder' %(PLUGINS,pathHdlr(PATHS['plugins.txt'])))
-    updatelink(PLUGINS, pathHdlr(PATHS['plugins.txt']))
+    updatelink(PLUGINS, pathHdlr(PATHS['plugins.txt']), log)
 
     print ('Parsing MO mods configuration')
-    MODS=os.path.join(pathHdlr(PATHS['mo']),'mods')
+    MODS=pathHdlr(PATHS['mods'])
     for modpath in [os.path.join(MODS,i[1:]).strip() for i in open(os.path.join(PDIR,'modlist.txt')).readlines() if i.startswith('+')]:
         addvfslayer(DATADIR,modpath, log)
     open('movfs4l_log.json','w').write(json.dumps(log, indent=4))
